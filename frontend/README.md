cb77c9bcf87cddb4e2ce7881bb470391eed20ed5
PranavKrishnaS <pksckochi@gmail.com>
2025-06-07 02:25:02 +0530

    3-connected frontend and backend

--- frontend/README.md
+++ frontend/README.md
@@ -1,5 +1,5 @@
 # React + Vite
-
+# 1:39:02
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,28 +1,62 @@
-import { Avatar, Box, Card, CardBody, CardHeader, Flex, Heading, IconButton, Text } from "@chakra-ui/react"
+import { Avatar, Box, Card, CardBody, CardHeader, Flex, Heading, IconButton, Text, useToast } from "@chakra-ui/react"
 import { BiTrash } from "react-icons/bi"
 import EditModal from "./EditModal"
+import { BASE_URL } from "../App"
 
-const UserCard = ({user}) => {
-  return (
+const UserCard = ({user, setUsers}) => {
+    const toast = useToast();
+    const handleDeleteUser = async() => {
+        try {
+            const res = await fetch(BASE_URL + "/friends/" + user.id, {
+                method: "DELETE",
+
+            })
+            const data = await res.json()
+            if(!res.ok){
+                throw new Error(data.error)
+            }
+            setUsers((prevUsers) => prevUsers.filter((u)=> u.id !== user.id))
+            toast({
+                title: "Sucess!",
+                status: "success",
+                duration: 2000,
+                position: "top-center",
+                description: "Friend deleted successfully."
+            })
+        } catch (error) {
+            toast({
+                title: "An error occured.",
+                status: "error",
+                description: error.message,
+                duration: 2000,
+                isClosable: true,
+                position: "top-center"
+            })
+        }
+    }
+    return (
     <Card>
         <CardHeader>
             <Flex gap={4}>
                 <Flex flex={"1"} gap={"4"} alignItems={"center"}>
                     { /* Avatar */}
-                    <Avatar src="https://avatar.iran.liara.run/public"/>
+                    <Avatar src={user.imgUrl}/>
                     <Box>
                         <Heading size={"sm"}>{user.name}</Heading>
                         <Text>{user.role}</Text>
                     </Box>
                 </Flex>
                 <Flex>
-                    <EditModal/>
+                    <EditModal
+                    user={user}
+                    setUsers={setUsers}/>
                     <IconButton 
                         variant='ghost'
                         colorScheme="red"
                         size={"sm"}
                         aria-label="See menu"
-                        icon={<BiTrash size={20}/>}
+                        icon={<BiTrash size={20}
+                        onClick={handleDeleteUser}/>}
                     />
                 </Flex>
             </Flex>
